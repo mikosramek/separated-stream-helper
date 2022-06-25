5e0b778e70477a9322b8b7b8d646c2d4eb83ee51
Miko Sramek <mikoggsramek@hotmail.com>
2022-06-25 18:02:05 -0400

    fix: poll overload
    - Updated socket handler to only poll music details if there are listeners

--- frontend/src/utils/SocketMusicHandler.js
+++ frontend/src/utils/SocketMusicHandler.js
@@ -4,18 +4,27 @@ class SocketMusicHandler {
     constructor(socketHandler) {
         this.socket = socketHandler;
         this.pollInterval = null;
+        this.currentEventListeners = 0;
     }
     init() {
-        console.log('initing socket music handler')
-        this.pollInterval = setInterval(() => this.pollBackEnd(), 5000); 
+        this.pollInterval = setInterval(() => {
+            if (this.currentEventListeners > 0) {
+                this.pollBackEnd();
+            }
+        }, 5000); 
         this.pollBackEnd(true);
     }
     destroy() {
         if (this.pollInterval) clearInterval(this.pollInterval);
+        this.currentEventListeners = 0;
     }
     registerInfoCallback(callback) {
         this.socket.registerEvent(EVENTS.music_current_response, callback);
-        return () => this.socket.clearEvent(EVENTS.music_current_response, callback)
+        this.currentEventListeners += 1;
+        return () => {
+            this.currentEventListeners -= 1;
+            this.socket.clearEvent(EVENTS.music_current_response, callback);
+        }
     }
     pollBackEnd(forced = false) {
         this.socket.emitEventOnce(forced ? EVENTS.music_current_request_forced : EVENTS.music_current_request);
